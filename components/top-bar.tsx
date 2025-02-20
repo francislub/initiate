c29294738d7eead082316a3665773272034c1129
Eng Lubanjwa Francis <lubanjwafrancis@gmail.com>
2025-02-19 22:53:52 -0800

    tiktok

--- components/top-bar.tsx
+++ components/top-bar.tsx
@@ -19,10 +19,23 @@ export default function TopBar() {
         </div>
         <div className="flex items-center space-x-4 flex-wrap justify-center">
           <div className="flex space-x-4">
-            <Link href="https://facebook.com" className="hover:text-gray-300 transition-colors">
+            <Link href="https://www.facebook.com/profile.php?id=61565856121815" className="hover:text-gray-300 transition-colors">
               <span className="sr-only">Facebook</span>
               <svg className="h-4 w-4" fill="currentColor" viewBox="0 0 24 24">
                 <path d="M22 12c0-5.523-4.477-10-10-10S2 6.477 2 12c0 4.991 3.657 9.128 8.438 9.878v-6.987h-2.54V12h2.54V9.797c0-2.506 1.492-3.89 3.777-3.89 1.094 0 2.238.195 2.238.195v2.46h-1.26c-1.243 0-1.63.771-1.63 1.562V12h2.773l-.443 2.89h-2.33v6.988C18.343 21.128 22 16.991 22 12z" />
+              </svg>
+            </Link>
+            <Link href="https://x.com/initiateug" className="hover:text-gray-300 transition-colors">
+              <span className="sr-only">X</span>
+              <svg className="h-4 w-4" fill="currentColor" viewBox="0 0 24 24">
+                <path d="M21.707 2.293a1 1 0 00-1.414 0L12 10.586 3.707 2.293a1 1 0 10-1.414 1.414L10.586 12l-8.293 8.293a1 1 0 101.414 1.414L12 13.414l8.293 8.293a1 1 0 001.414-1.414L13.414 12l8.293-8.293a1 1 0 000-1.414z"/>
+              </svg>
+            </Link>
+
+            <Link href="https://www.tiktok.com/@initiate375" className="hover:text-gray-300 transition-colors">
+              <span className="sr-only">TikTok</span>
+              <svg className="h-4 w-4" fill="currentColor" viewBox="0 0 24 24">
+                <path d="M12 2c1.434 2.937 3.689 5.083 7 5.252v2.025a8.376 8.376 0 01-4.129-1.114c.016 2.094.014 4.212.014 6.324 0 .747-.004 1.484-.014 2.212a4.86 4.86 0 01-4.846 4.846A4.862 4.862 0 015.18 16.64a4.86 4.86 0 014.846-4.846c.15 0 .299.007.447.019V15.6a2.369 2.369 0 00-.447-.043 2.375 2.375 0 102.375 2.375c0-.7.003-1.396.01-2.085.02-2.167.022-4.34-.01-6.513V2H12z"/>
               </svg>
             </Link>
             <Link href="https://twitter.com" className="hover:text-gray-300 transition-colors">
@@ -31,18 +44,18 @@ export default function TopBar() {
                 <path d="M8.29 20.251c7.547 0 11.675-6.253 11.675-11.675 0-.178 0-.355-.012-.53A8.348 8.348 0 0022 5.92a8.19 8.19 0 01-2.357.646 4.118 4.118 0 001.804-2.27 8.224 8.224 0 01-2.605.996 4.107 4.107 0 00-6.993 3.743 11.65 11.65 0 01-8.457-4.287 4.106 4.106 0 001.27 5.477A4.072 4.072 0 012.8 9.713v.052a4.105 4.105 0 003.292 4.022 4.095 4.095 0 01-1.853.07 4.108 4.108 0 003.834 2.85A8.233 8.233 0 012 18.407a11.616 11.616 0 006.29 1.84" />
               </svg>
             </Link>
-            <Link href="https://linkedin.com" className="hover:text-gray-300 transition-colors">
+            {/* <Link href="https://linkedin.com" className="hover:text-gray-300 transition-colors">
               <span className="sr-only">LinkedIn</span>
               <svg className="h-4 w-4" fill="currentColor" viewBox="0 0 24 24">
                 <path d="M20.447 20.452h-3.554v-5.569c0-1.328-.027-3.037-1.852-3.037-1.853 0-2.136 1.445-2.136 2.939v5.667H9.351V9h3.414v1.561h.046c.477-.9 1.637-1.85 3.37-1.85 3.601 0 4.267 2.37 4.267 5.455v6.286zM5.337 7.433c-1.144 0-2.063-.926-2.063-2.065 0-1.138.92-2.063 2.063-2.063 1.14 0 2.064.925 2.064 2.063 0 1.139-.925 2.065-2.064 2.065zm1.782 13.019H3.555V9h3.564v11.452zM22.225 0H1.771C.792 0 0 .774 0 1.729v20.542C0 23.227.792 24 1.771 24h20.451C23.2 24 24 23.227 24 22.271V1.729C24 .774 23.2 0 22.222 0h.003z" />
               </svg>
-            </Link>
-            <Link href="https://instagram.com" className="hover:text-gray-300 transition-colors">
+            </Link> */}
+            {/* <Link href="https://instagram.com" className="hover:text-gray-300 transition-colors">
               <span className="sr-only">Instagram</span>
               <svg className="h-4 w-4" fill="currentColor" viewBox="0 0 24 24">
                 <path d="M12 2.163c3.204 0 3.584.012 4.85.07 3.252.148 4.771 1.691 4.919 4.919.058 1.265.069 1.645.069 4.849 0 3.205-.012 3.584-.069 4.849-.149 3.225-1.664 4.771-4.919 4.919-1.266.058-1.644.07-4.85.07-3.204 0-3.584-.012-4.849-.07-3.26-.149-4.771-1.699-4.919-4.92-.058-1.265-.07-1.644-.07-4.849 0-3.204.013-3.583.07-4.849.149-3.227 1.664-4.771 4.919-4.919 1.266-.057 1.645-.069 4.849-.069zm0-2.163c-3.259 0-3.667.014-4.947.072-4.358.2-6.78 2.618-6.98 6.98-.059 1.281-.073 1.689-.073 4.948 0 3.259.014 3.668.072 4.948.2 4.358 2.618 6.78 6.98 6.98 1.281.058 1.689.072 4.948.072 3.259 0 3.668-.014 4.948-.072 4.354-.2 6.782-2.618 6.979-6.98.059-1.28.073-1.689.073-4.948 0-3.259-.014-3.667-.072-4.947-.196-4.354-2.617-6.78-6.979-6.98-1.281-.059-1.69-.073-4.949-.073zm0 5.838c-3.403 0-6.162 2.759-6.162 6.162s2.759 6.163 6.162 6.163 6.162-2.759 6.162-6.163c0-3.403-2.759-6.162-6.162-6.162zm0 10.162c-2.209 0-4-1.79-4-4 0-2.209 1.791-4 4-4s4 1.791 4 4c0 2.21-1.791 4-4 4zm6.406-11.845c-.796 0-1.441.645-1.441 1.44s.645 1.44 1.441 1.44c.795 0 1.439-.645 1.439-1.44s-.644-1.44-1.439-1.44z" />
               </svg>
-            </Link>
+            </Link> */}
             <Link href="https://youtube.com" className="hover:text-gray-300 transition-colors">
               <span className="sr-only">YouTube</span>
               <svg className="h-4 w-4" fill="currentColor" viewBox="0 0 24 24">
